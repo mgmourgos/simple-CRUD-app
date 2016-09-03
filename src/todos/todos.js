ecad54680db917dfc5a36d935c0054f5dae42fe5
mgmourogs@users.noreply.github.com <mgmourgos@users.noreply.github.com>
2016-09-02 23:14:02 -0700

    Added create task functionality

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,7 +1,11 @@
 export default function($scope) {
+  let params = {
+    createHasInput: false
+  };
+
   $scope.todos = [
     {
-      task: 'do dished',
+      task: 'do dishes',
       isCompleted: false
     },
     {
@@ -13,4 +17,24 @@ export default function($scope) {
   $scope.onCompletedClick = (todo) => {
     todo.isCompleted = !todo.isCompleted;
   };
+
+  $scope.createTask = () => {
+    params.createHasInput = false;
+    $scope.createTaskInput = '';
+  };
+
+  $scope.$watch('createTaskInput', val => {
+    if(!val && params.createHasInput) {
+      $scope.todos.pop();
+      params.createHasInput = false;
+    } else if(val && !params.createHasInput) {
+      $scope.todos.push({ task: val, isCompleted: false});
+      params.createHasInput = true;
+    } else if (val && params.createHasInput) {
+      $scope.todos[$scope.todos.length - 1].task = val;
+
+    }
+  });
+
+
 }
